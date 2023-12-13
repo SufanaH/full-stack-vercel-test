f86a78f2f6e77f27d861e35d3f33aee8e9dd7601
SufanaH <sufana_1993@hotmail.com>
2023-12-13 07:43:56 +0300

    Atlas Mongodb commit

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const connectDB = require("../config/db");
 
 const app = express();
 
-const PORT = process.env.SERVER_PORT || 3038
+const PORT = process.env.PORT || 3038
 const URL = (process.env.MONGODB_URL|| 'mongodb+srv://Sufana:sufana123@cluster0.zuslhqs.mongodb.net/full-stack-vercel-test?retryWrites=true&w=majority')
 
 
--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const dev ={
     app:{port : Number(process.env.SERVER_PORT) || 3037},
-    db:{url: process.env.MOGODB_URL || 'mongodb+srv://Sufana:sufana123@cluster0.zuslhqs.mongodb.net/full-stack-vercel-test?retryWrites=true&w=majority',},
+    db:{url: process.env.MONGODB_URL || 'mongodb+srv://Sufana:sufana123@cluster0.zuslhqs.mongodb.net/full-stack-vercel-test?retryWrites=true&w=majority',},
 };
 
 module.exports = dev;
